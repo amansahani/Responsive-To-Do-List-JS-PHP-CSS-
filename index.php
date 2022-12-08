42168460d356aeb23811ff9a6b2f58d2c2d454fc
Aman Sahani <66307331+amansahani@users.noreply.github.com>
2022-12-08 22:54:03 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+
+<head>
+  <meta charset="utf-8">
+  <title>To Do List</title>
+  <?php echo " <link rel='stylesheet' src='style.css'>" ?>
+</head>
+
+<body>
+
+  <?php
+  $name = "Aryan";
+  echo ("<h1 style='background-color:blue'>Hello $name</h1>");
+  echo "<hr>";
+  echo "<p> Hello Bachha </p>";
+  ?>
+
+</body>
+
+</html>
